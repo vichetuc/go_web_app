f222c647f3200d9479c5ce2dadd3d3db8a30ed88
Julio Farah <julio.farah@gmail.com>
2015-06-01 18:46:40 -0300

    moved html content to separate files

--- hello.go
+++ hello.go
@@ -1,31 +1,29 @@
 package go_web_app
-
 import (
-	"net/http"
-	"fmt"
 	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	guestbookForm []byte
+	signTemplate = template.Must(template.ParseFiles("views/guestbook.html"))
 )
 
 func init() {
+	content, err := ioutil.ReadFile("views/guestbookform.html")
+
+	if err != nil {
+		panic(err)
+	}
+
+	guestbookForm = content
+
 	http.HandleFunc("/", root)
 	http.HandleFunc("/sign", sign)
-
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, guestbookForm)
-}
-
-const guestbookForm = `
-	<html>
-		<body>
-			<form actions="/sign" method="post">
-				<div><textarea name="content" rows="3" cols="60"></textarea></div>
-				<div><input type="submit" value="Sign Guestbook"></div>
-			</form>
-		</body>
-	</html>
-`
+func root(w http.ResponseWriter, r *http.Request) { w.Write(guestbookForm) }
 
 func sign(w http.ResponseWriter, r *http.Request) {
 	err := signTemplate.Execute(w, r.FormValue("content"))
@@ -33,14 +31,3 @@ func sign(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
-
-const signTemplateHTML = `
-	<html>
-		<body>
-			<p>You wrote:</p>
-			<pre>{{.}}</pre>
-		</body>
-	</html>
-`
-
-var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
