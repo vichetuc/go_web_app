71e2255c7309c16faafb4f92cbc86505b659ad8e
Julio Farah <julio.farah@gmail.com>
2015-06-05 19:16:08 -0300

    wip #3 - add views/guestbook and parse HTML file ihull!

--- api/controllers/home.go
+++ api/controllers/home.go
@@ -9,37 +9,17 @@ import (
 	"html/template"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	context := appengine.NewContext(r)
-	query := datastore.NewQuery("Greeting").Ancestor(keys.GuestbookKey(context)).Order("-Date").Limit(10)
-	greetings := make([]models.Greeting, 0, 10)
-	if _, err := query.GetAll(context, &greetings); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := guestbookTemplate.Execute(w, greetings); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
+var templates = template.Must(template.ParseGlob("../api/views/*"))
 
-var guestbookTemplate = template.Must(template.New("book").Parse(`
-<html>
-  <head>
-    <title>Go Guestbook</title>
-  </head>
-  <body>
-    {{range .}}
-      {{with .Author}}
-        <p><b>{{.}}</b> wrote:</p>
-      {{else}}
-        <p>An anonymous person wrote:</p>
-      {{end}}
-      <pre>{{.Content}}</pre>
-    {{end}}
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`))
+func Home(w http.ResponseWriter, r *http.Request) {
+  context := appengine.NewContext(r)
+  query := datastore.NewQuery("Greeting").Ancestor(keys.GuestbookKey(context)).Order("-Date").Limit(10)
+  greetings := make([]models.Greeting, 0, 10)
+  if _, err := query.GetAll(context, &greetings); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+  if err := templates.ExecuteTemplate(w, "guestbookPage", greetings); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
